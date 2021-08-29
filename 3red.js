2ea3cdb77bd3e550fc8dc6e2f55d991607090857
romans1995 <cboby26@gmail.com>
2021-08-29 19:56:53 +0300

    new fixed isseus

--- 3red.js
+++ 3red.js
@@ -6,16 +6,9 @@ if(num > 0){
 }else{
     absolute = num +(num * -2);
 }
-
 console.log(absolute)
 // output 8
-
-
-
-
 // or 
-
-
 let absolute;
 // your code goes here
 if(num < 0){
@@ -23,13 +16,9 @@ if(num < 0){
 }else{
     absolute = num
 }
-
 console.log(absolute)
 // output 8
-
 // or 
-
-
 absolute = num < 0 ? -num : num;
 console.log(absolute);
 
