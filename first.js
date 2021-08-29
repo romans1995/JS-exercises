7e6eaddb99d9b9bb9f8a799af3a60081d70cc6c9
romans1995 <44429106+romans1995@users.noreply.github.com>
2021-08-29 17:35:39 +0300

    Add files via upload

---
+++ first.js
@@ -0,0 +1,17 @@
+const num = 123
+let sum = 0
+// your code goes here
+
+
+var value2 = 300
+var value3 = 431;
+var value = 251;
+while (value) {
+    sum += value % 10;
+    value = Math.floor(value / 10);
+}
+// for value 
+console.log(sum);
+// answer is 8     
+
+
