8cd6c05ebdaa9dad819ac1f102e02f4dacfe366a
romans1995 <cboby26@gmail.com>
2021-08-31 09:29:15 +0300

    finish

--- number-four.js
+++ number-four.js
@@ -1,5 +1,6 @@
 const grades = [73, 91, 88, 56, 62, 84, 95, 96, 88, 92];
 let maximum = 0;  
+// first mission 
    const max =(grades) =>{
     for(let i = 0; i<grades.length;i++){
         if(maximum < grades[i]){
@@ -8,38 +9,40 @@ let maximum = 0;
     }
     return maximum;
    }
+
 const min = (grades) => {
     let minimum = grades[0]; 
      minimum = grades[0];
-    console.log(minimum);
     for(let i = 0 ; i < grades.length ; i++){
         if(minimum > grades[i]){
             minimum = grades[i];
         }
-        console.log(minimum);
     }
+    return minimum;
 }
- console.log(minimum) ;
- let range  = max(grades) - minimum;
- console.log(range);
 
+ console.log(min(grades)) ;
+ let range  = max(grades) - min(grades);
+ console.log(range);
+// second Mission 
  const gradesSorted = [...grades].sort();
 let median = gradesSorted[gradesSorted.length/2];
 median;
-
+// thired mission 
 let halfRanged = grades.slice(grades.length/2);
 console.log(halfRanged);
 
 const maxh =(halfRanged) =>{
     for(let i = 0; i < halfRanged.length; i++){
         if(maximum < halfRanged[i]){
-            maximum =halfRanged[i];
+            maximum = halfRanged[i];
         }
     }
     return maximum;
    }
    console.log(maxh(halfRanged));
    const minh =(halfRanged) =>{
+    minimum =halfRanged[0];
     for(let i = 0; i < halfRanged.length; i++){
         if(minimum> halfRanged[i]){
             minimum =halfRanged[i];
@@ -48,29 +51,7 @@ const maxh =(halfRanged) =>{
     return minimum;
    }
    console.log(minh(halfRanged));
-   let halfRanged = (maxh(halfRanged)- minimum);
+   halfRanged = (maxh(halfRanged)- minimum);
    console.log(halfRanged);
 
-   let halfRanged = grades.slice(grades.length/2);
-console.log(halfRanged);
-const maxh =(halfRanged) =>{
-    for(let i = 0; i < halfRanged.length; i++){
-        if(maximum < halfRanged[i]){
-            maximum =halfRanged[i];
-        }
-    }
-    return maximum;
-   }
-   console.log(maxh(halfRanged));
-   const minh = (halfRanged) =>{
-    for(let i = 0; i < halfRanged.length; i++){
-        if(minimum> halfRanged[i]){
-            minimum =halfRanged[i];
-        }
-    }
-    return minimum;
-   }
-
-   console.log(minh(halfRanged));
-   let halfRanged = (maxh(halfRanged)- minh(minimum));
-   console.log(halfRanged);
+    
