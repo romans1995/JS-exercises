7bd34718271459aaff40e637dce27713d0816bc1
romans1995 <cboby26@gmail.com>
2021-08-31 09:43:27 +0300

    Last finish

--- number-four.js
+++ number-four.js
@@ -1,3 +1,22 @@
+// You are given an array of a student's grades during the school year. Your task is to compute the following statistics:
+// The range of the grades (difference between highest and lowest)
+// The median grade (the grade in the middle)
+// The range of the grades from the second half of the school year
+// Here's a template you can use:
+// const grades = [73, 91, 88, 56, 62, 84, 95, 96, 88, 92]
+// // your code goes here
+// console.log(...)
+
+// The output of your code (what console.log prints) should be in the following format (notice the line-breaks and indentation):
+// Stats:
+//     Range: 40
+//     Median: 88
+//     Half Range: 12
+// Test your code on different arrays of different sizes.
+// Separate your code into functions! Avoid code duplication!
+
+// Submit this code through your JS GitHub repo. Split your work into several commits.
+
 const grades = [73, 91, 88, 56, 62, 84, 95, 96, 88, 92];
 let maximum = 0;  
 // first mission 
@@ -30,28 +49,5 @@ let median = gradesSorted[gradesSorted.length/2];
 median;
 // thired mission 
 let halfRanged = grades.slice(grades.length/2);
-console.log(halfRanged);
-
-const maxh =(halfRanged) =>{
-    for(let i = 0; i < halfRanged.length; i++){
-        if(maximum < halfRanged[i]){
-            maximum = halfRanged[i];
-        }
-    }
-    return maximum;
-   }
-   console.log(maxh(halfRanged));
-   const minh =(halfRanged) =>{
-    minimum =halfRanged[0];
-    for(let i = 0; i < halfRanged.length; i++){
-        if(minimum> halfRanged[i]){
-            minimum =halfRanged[i];
-        }
-    }
-    return minimum;
-   }
-   console.log(minh(halfRanged));
-   halfRanged = (maxh(halfRanged)- minimum);
-   console.log(halfRanged);
-
-    
+halfRanged = max(halfRanged) - min(halfRanged)
+console.log(halfRanged)
